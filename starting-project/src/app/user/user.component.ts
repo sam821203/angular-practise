bffd6d61d0530f430ad762131a1187eb050f34a1
SamHuang <sam821203@yahoo.com.tw>
2024-11-12 22:20:09 +0800

    complete: chapter 37

--- starting-project/src/app/user/user.component.ts
+++ starting-project/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter, output } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -10,5 +10,31 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
+  // selectedUser = signal(DUMMY_USERS[randomIndex]);
+  // imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
+
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter<string>();
+  // 下面的用法寫得更新更短，但沒有像 input signal 一樣
+  // select = output<string>();
+
+  // signal 用法
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
+
+  // imagePath = computed(() => {
+  //   return 'assets/users/' + this.avatar;
+  // });
+
+  onSelectUser() {
+    // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    // this.selectedUser.set(DUMMY_USERS[randomIndex]);
+    this.select.emit(this.id);
+  }
 }
