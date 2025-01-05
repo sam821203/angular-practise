102fe9b571849ca7960b4ecbc6b8b26a8fff9b9a
SamHuang <sam821203@yahoo.com.tw>
2025-01-05 18:08:57 +0800

    complete: chapter rxjs

---
+++ rxjs-starting-project/src/app/app.component.ts
@@ -0,0 +1,74 @@
+import {
+  Component,
+  computed,
+  DestroyRef,
+  effect,
+  inject,
+  OnInit,
+  signal,
+} from '@angular/core';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { interval, map, Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+})
+export class AppComponent implements OnInit {
+  clickCount = signal(0);
+  clickCount$ = toObservable(this.clickCount);
+  interval$ = interval(1000);
+  intervalSignal = toSignal(this.interval$, { initialValue: 0 });
+  // interval = signal(0);
+  // doubleInterval = computed(() => this.interval() * 2)
+  customInterval$ = new Observable((subscriber) => {
+    let timesExecuted = 0;
+    const interval = setInterval(() => {
+      subscriber.error();
+      if (timesExecuted > 3) {
+        clearInterval(interval);
+        subscriber.complete();
+        return;
+      }
+      console.log('Emitting new value');
+      subscriber.next({ message: 'New Value' });
+      timesExecuted++;
+    }, 2000);
+  });
+  private destroyRef = inject(DestroyRef);
+
+  constructor() {
+    // effect(() => {
+    //   console.log(`Click button ${this.clickCount()} times`);
+    // });
+  }
+
+  ngOnInit() {
+    // const subscription = interval(1000)
+    //   .pipe(map((val) => val * 2))
+    //   .subscribe({
+    //     next: (val) => console.log(val),
+    //   });
+    // this.destroyRef.onDestroy(() => {
+    //   subscription.unsubscribe();
+    // });
+    this.customInterval$.subscribe({
+      next: (val) => console.log(val),
+      complete: () => console.log('COMPLETED!'),
+      error: (err) => console.log(err),
+    });
+
+    const subscription = this.clickCount$.subscribe({
+      next: (val) => console.log(`Click button ${this.clickCount()} times`),
+    });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
+
+  onClick() {
+    this.clickCount.update((prevCount) => prevCount + 1);
+  }
+}
