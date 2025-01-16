f2ccd94dfbd2cbe9d3562b527e367e89206f7ba7
SamHuang <sam821203@yahoo.com.tw>
2025-01-16 23:05:09 +0800

    complete: chapter 243

---
+++ form-starting-project/src/app/auth/login/login.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+})
+export class LoginComponent {
+  onSubmit(formData: NgForm) {
+    console.log(formData.form);
+
+    if (formData.form.invalid) return;
+    const enteredEmail = formData.form.value.email;
+    const enteredPassword = formData.form.value.password;
+    console.log(formData.form);
+
+    console.log(enteredEmail, enteredPassword);
+  }
+}
