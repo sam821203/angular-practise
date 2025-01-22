7af854c77ebd3587256af13b9a1b947702ebff57
SamHuang <sam821203@yahoo.com.tw>
2025-01-22 22:46:52 +0800

    complete: chapter 278

---
+++ route-starting-project/src/app/users/user-tasks/user-tasks.component.ts
@@ -0,0 +1,34 @@
+import { Component, computed, DestroyRef, inject, input } from '@angular/core';
+import { UsersService } from '../users.service';
+import { ActivatedRoute, RouterLink, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-user-tasks',
+  standalone: true,
+  imports: [RouterOutlet, RouterLink],
+  templateUrl: './user-tasks.component.html',
+  styleUrl: './user-tasks.component.css',
+})
+export class UserTasksComponent {
+  // userId = input.required<string>();
+  userName = '';
+  private userService = inject(UsersService);
+  private activatedRoute = inject(ActivatedRoute);
+  private destroyRef = inject(DestroyRef);
+  // userName = computed(
+  //   () => this.userService.users.find((u) => u.id === this.userId())?.name
+  // );
+
+  ngOnInit(): void {
+    console.log(this.activatedRoute);
+    const subscription = this.activatedRoute.paramMap.subscribe({
+      next: (paramMap) => {
+        this.userName =
+          this.userService.users.find((u) => u.id === paramMap.get('userId'))
+            ?.name || '';
+      },
+    });
+
+    this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  }
+}
