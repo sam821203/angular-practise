d2478c69664e7ef8b600b2df06a7954607dd8fce
SamHuang <sam821203@yahoo.com.tw>
2025-05-18 23:53:55 +0800

    feat: style invalid input fields

--- ultimate-courses/angular-basics-seed-main/src/app/app.component.ts
+++ ultimate-courses/angular-basics-seed-main/src/app/app.component.ts
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
       <header class="header">
         <img src="/assets/img/logo.svg" alt="Ultimate Donuts" />
       </header>
-      <donut-list></donut-list>
+      <!-- <donut-list></donut-list> -->
+      <donut-single></donut-single>
     </div>
   `,
   styles: [
